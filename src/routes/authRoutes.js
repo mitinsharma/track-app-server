b548ac3078d58938b5f4011c2756ae8ae8139123
Mitin Sharma <er.mitinsharma@gmail.com>
2021-04-18 22:12:37 -0400

    signin and signup api's created

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -16,5 +16,23 @@ router.post('/signup', async (req,res) => {
         return res.status(422).send(err);
     }
 });
+
+router.post('/signin', async (req, res) => {
+    const {email, password} = req.body;
+    if(!email || !password) {
+        return res.status(422).send({ error: 'Must provide email and password!'});
+    }
+    if(!user) {
+        return res.status(422).send({ error: 'Invalid password or email'});
+    }
+    try{
+        console.log('yooo---');
+        await user.comparePassword(password);
+        const token = jwt.sign({ userID: user._id },'MY_SECRET_KEY');
+        res.send({token});
+    } catch(err) {
+        return res.status(422).send({ error: 'Invalid password or email' });
+    }
+});
  
 module.exports = router;
